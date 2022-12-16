52509bb56cdd217d436e275fbdea6e494e7e3ac1
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2022-12-16 17:10:26 +0900

    add amplify auth and predications category

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "texttospeech09ce59d5": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "predictions": {
+        "speechGenerator9701e6fd": {
+            "region": "string",
+            "language": "string",
+            "voice": "string"
+        }
+    }
+}
